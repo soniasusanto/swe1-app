a2404a82ab25c727c83dc3a103116a1c8337a8ff
Sonia Susanto <soniasusanto@gmail.com>
2024-10-12 20:46:48 -0400

    add travis and other libs

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class PollsPageTest(TestCase):
+    def test_pollspage_status_code(self):
+        response = self.client.get(reverse('polls:index'))
+        self.assertEqual(response.status_code, 200)
